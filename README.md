e6dd89972ebc0941e39743512315bec1f1dab293
Hilfi M. Alkaff <alkaff2@illinois.edu>
2014-03-09 22:36:00 -0500

    Able to specify number of output bolts and number of tasks per bolt.

--- README.md
+++ README.md
@@ -4,13 +4,15 @@
 
 # Running experiments
 
-    $ ./scripts/run.sh TestTopology <options>
+    $ ./scripts/run.sh <options>
 
 The following options are available.
 * --name=<Name of Topology>
-* --num-spout=<number of spout>
-* --io-ratio=<Ratio if data coming in/out to/of bolt>
+* --numSpout=<number of spout>
+* --ioRatio=<Ratio if data coming in/out to/of bolt>
 * --children=<Number of children each bolt has>
 * --depth=<Depth of the topology>
+* --numOutputBolt=<Number of output bolts>
+* --numTask=<Number of task per bolt>
 
 This project is being adapted from https://github.com/nathanmarz/storm-starter
--- src/jvm/storm/starter/TestTopology.java
+++ src/jvm/storm/starter/TestTopology.java
@@ -14,10 +14,10 @@ public class TestTopology {
 	@Option(name = "--name", usage = "Name for the topology")
 	private static String topologyName = "TestTopology";
 
-	@Option(name = "--num-spout", usage = "Number of spouts in the topology")
+	@Option(name = "--numSpout", usage = "Number of spouts in the topology")
 	private static int _numSpout = 1;
 
-	@Option(name = "--io-ratio", usage = "Ratio of input to output in the bolts")
+	@Option(name = "--ioRatio", usage = "Ratio of input to output in the bolts")
 	private static double _inputOutputRatio = 1.0;
 
 	@Option(name = "--children", usage = "Number of children each node has")
@@ -29,37 +29,43 @@ public class TestTopology {
 	@Option(name = "--depth", usage = "How deep is the tree")
 	private static int _depth = 1;
 
+	@Option(name = "--numOutputBolt", usage = "Number of output bolts")
+	private static int _numOutputBolt = 1;
+
+	@Option(name = "--numTask", usage = "Number of tasks per bolt")
+	private static int _numTask = 3;
+
 	public void run() throws Exception {
 		TopologyBuilder builder = new TopologyBuilder();
 
 		/* Setting up spouts */
 		for (int i = 0; i < _numSpout; i++) {
-			builder.setSpout("spout " + String.valueOf(i), new TestSpout(), 3);
+			builder.setSpout("spout_" + String.valueOf(i), new TestSpout(),
+					_numTask);
 		}
 
 		/* Bolts (depth 0) connected to spout */
 		for (int i = 0; i < _children; i++) {
-			String boltName = "bolt 0 " + String.valueOf(i);
+			String boltName = "bolt_1_" + String.valueOf(i);
 			BoltDeclarer bolt = builder.setBolt(boltName, new TestBolt(
-					_inputOutputRatio), 3);
+					_inputOutputRatio), _numTask);
 
 			for (int j = 0; j < _numSpout; j++) {
-				String spoutName = "spout " + String.valueOf(j);
+				String spoutName = "spout_" + String.valueOf(j);
 				bolt.shuffleGrouping(spoutName);
 			}
 		}
 
 		/* Bolts connected to previous bolts */
-		for (int i = 1; i < _depth; i++) {
-			for (int j = 0; j < _children; j++) {
-				String boltName = "bolt " + String.valueOf(i) + " "
+		for (int i = 2; i <= _depth; i++) {
+			for (int j = 0; j < Math.pow(_children, i); j++) {
+				String boltName = "bolt_" + String.valueOf(i) + "_"
 						+ String.valueOf(j);
-
 				BoltDeclarer bolt = builder.setBolt(boltName, new TestBolt(
-						_inputOutputRatio), 3);
+						_inputOutputRatio), _numTask);
 
-				for (int k = 0; k < _children; k++) {
-					String prevBoltName = "bolt " + String.valueOf(i) + " "
+				for (int k = 0; k < Math.pow(_children, i - 1); k++) {
+					String prevBoltName = "bolt_" + String.valueOf(i - 1) + "_"
 							+ String.valueOf(k);
 
 					bolt.shuffleGrouping(prevBoltName);
@@ -67,9 +73,23 @@ public class TestTopology {
 			}
 		}
 
+		/* Output bolts */
+		for (int i = 0; i < _numOutputBolt; i++) {
+			String boltName = "output_bolt_" + String.valueOf(i);
+			BoltDeclarer outputBolt = builder.setBolt(boltName, new TestBolt(
+					_inputOutputRatio), _numTask);
+
+			for (int j = 0; j < Math.pow(_children, _depth); j++) {
+				String prevBoltName = "bolt_" + String.valueOf(_depth) + "_"
+						+ String.valueOf(j);
+
+				outputBolt.shuffleGrouping(prevBoltName);
+			}
+		}
+
 		Config conf = new Config();
 		conf.setDebug(true);
-		conf.setNumWorkers(3);
+		conf.setNumWorkers(_numTask);
 
 		StormSubmitter.submitTopology(topologyName, conf,
 				builder.createTopology());
